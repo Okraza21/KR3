8acc2314d9f10fc84da004ad6d1d54644a8bf9a1
Sergey Azarko <sokraza73@gmail.com>
2022-03-04 01:32:45 +0300

    2 commit

--- KR3_1.py
+++ KR3_1.py
@@ -1,32 +1,3 @@
-def kard():
-    while True:
-        try:
-            num = int(input('Введите номер карты из 12 цифр: '))
-            if len(str(num)) != 12:
-                print('Неверный ввод! Повторите попытку!')
-            else:
-                break
-        except ValueError:
-            print('Неверный ввод! Введите цифры!')
-    rez = []
-    for i in str(num):
-        rez.append(int(i))
-    for j in range(8):
-        rez[j] = '*'
-    for h in rez:
-        ' '.join(str(h))
-        print(h, end=' ')
-
-def polindrom():
-    while True:
-        slovo = input('Введиете слово: ')
-        if slovo.isalpha():
-            break
-    if slovo == slovo[::-1]:
-        print('Слово - полиндром!')
-    else:
-        print('Слово не полиндром(')
-
 # Класс Tomato:
 # 1. Создайте класс Tomato
 # 2. Создайте статическое свойство states, которое будет содержать все стадии
@@ -38,26 +9,6 @@ def polindrom():
 # созревания
 # 5. Создайте метод is_ripe(), который будет проверять, что томат созрел (достиг
 # последней стадии созревания)
-
-
-
-# class Tomato:
-#     states = {'Зелёный': 1, 'Жёлтый': 2, 'Красный': 3}
-#     def __init__(self, a=input('Введите цвет томата "Зелёный", "Жёлтый", "Красный": ')):
-#         self._index = a
-#         self._state = Tomato.states['Зелёный']
-#     def grow(self):
-#         return Tomato.states[self._index]
-#
-#     def is_ripe(self):
-#         if Tomato.grow(self) == 3:
-#             print('Томат созрел')
-#         else:
-#             print('Томат ещё не созрел')
-# tomat = Tomato()
-# tomat.is_ripe()
-
-
 class Tomato:
     states = {'Зелёный': 1, 'Жёлтый': 2, 'Красный': 3}
     def __init__(self, a=input('Какого цвета томат? : "Зелёный", "Жёлтый", "Красный"\nВвод: ')):
