96eafb72425ced61b68db04b4167055c83091873
Sergey Azarko <sokraza73@gmail.com>
2022-03-04 03:05:08 +0300

    4 commit

--- KR3_1.py
+++ KR3_1.py
@@ -1,18 +1,33 @@
+# Класс Tomato:
+# 1. Создайте класс Tomato
+# 2. Создайте статическое свойство states, которое будет содержать все стадии
+# созревания помидора
+# 3. Создайте метод __init__(), внутри которого будут определены два динамических
+# protected свойства: 1) _index - передается параметром и 2) _state - принимает первое
+# значение из словаря states
+# 4. Создайте метод grow(), который будет переводить томат на следующую стадию
+# созревания
+# 5. Создайте метод is_ripe(), который будет проверять, что томат созрел (достиг
+# последней стадии созревания
 class Tomato:
-    states = {'Зелёный': 1, 'Жёлтый': 2, 'Красный': 3}
-    def __init__(self, a=input('Какого цвета томат? : "Зелёный", "Жёлтый", "Красный"\nВвод: ')):
+    states = {1: 'Зелёный', 2: 'Жёлтый', 3: 'Красный'}
+    def __init__(self, a):
         self._index = a
-        self._state = Tomato.states['Зелёный']
+        self._state = Tomato.states[1]
     def grow(self):
         return Tomato.states[self._index]
 
     def is_ripe(self):
-        if Tomato.grow(self) == 3:
+        if Tomato.grow(self) == 'Красный':
             print('Томат созрел')
         else:
             print('Томат ещё не созрел')
-tomat = Tomato()
-tomat.is_ripe()
+tomat1 = Tomato(1)
+tomat2 = Tomato(2)
+tomat3 = Tomato(3)
+tomat1.is_ripe()
+tomat2.is_ripe()
+tomat3.is_ripe()
 
 # Класс TomatoBush
 # 1. Создайте класс TomatoBush
@@ -29,6 +44,16 @@ tomat.is_ripe()
 class TomatoBush:
     def __init__(self, kol_tomat):
         self.kol_tomat = kol_tomat
+        self.tomatoes = [tomat1, tomat2, tomat3]
+    def grow_all(self):
+        if tomat1 != 3:
+            tomat1 += 1
+    def all_are_ripe(self):
+        if TomatoBush.grow_all(self) == 3:
+            return True
+    def give_away_all(self):
+        pass
+
 
 
 
